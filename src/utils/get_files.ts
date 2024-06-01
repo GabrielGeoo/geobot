217d71abecc7f2f49db87c01309b87dc92955f24
Zilba26 <basile.lebaudy@gmail.com>
2024-06-02 00:19:11 +0200

    .

--- src/utils/get_files.ts
+++ src/utils/get_files.ts
@@ -1,8 +1,9 @@
 import * as fs from "fs";
 import path from "path";
 
-export default function getFiles(dir: string, extension: string | null = "ts", complete = true, recursive = true): string[] {
+export default function getFiles(dir: string, extensions: string | null | string[] = ["ts", "js"], complete = true, recursive = true): string[] {
   const fileNames: string[] = [];
+  if (typeof extensions == "string") extensions = [extensions];
 
   try {
     const files = fs.readdirSync(dir, { withFileTypes: true });
@@ -10,8 +11,8 @@ export default function getFiles(dir: string, extension: string | null = "ts", c
     console.log("For directory:", dir);
     files.forEach(file => {
       if (file.isDirectory()) {
-        fileNames.push(...getFiles(path.join(dir, file.name), extension, complete, recursive));
-      } else if (extension == null || file.name.endsWith(`.${extension}`)) {
+        fileNames.push(...getFiles(path.join(dir, file.name), extensions, complete, recursive));
+      } else if (extensions == null || extensions.some(extension => file.name.endsWith(extension))) {
         if (complete) fileNames.push(path.join(dir, file.name));
         else fileNames.push(file.name);
       }
