2d3f7790888400dfbe57f5fec1324540081ad01c
Zilba26 <basile.lebaudy@gmail.com>
2024-06-01 20:37:57 +0200

    .

--- src/utils/get_files.ts
+++ src/utils/get_files.ts
@@ -4,14 +4,20 @@ import path from "path";
 export default function getFiles(dir: string, extension: string | null = "ts", complete = true, recursive = true): string[] {
   const fileNames: string[] = [];
 
-  fs.readdirSync(dir, {withFileTypes: true, recursive: recursive}).forEach(file => {
-    if (file.isDirectory()) {
-      fileNames.push(...getFiles(path.join(dir, file.name), extension, complete, recursive));
-    } else if (extension == null || file.name.endsWith(`.${extension}`)) {
-      if (complete) fileNames.push(path.join(dir, file.name));
-      else fileNames.push(file.name);
-    }
-  });
+  try {
+    const files = fs.readdirSync(dir, { withFileTypes: true });
+    files.forEach(file => {
+      if (file.isDirectory()) {
+        fileNames.push(...getFiles(path.join(dir, file.name), extension, complete, recursive));
+      } else if (extension == null || file.name.endsWith(`.${extension}`)) {
+        if (complete) fileNames.push(path.join(dir, file.name));
+        else fileNames.push(file.name);
+      }
+    });
+  } catch (error) {
+    console.error(`Error reading directory ${dir}:`, error);
+  }
 
+  console.log("Files found:", fileNames);
   return fileNames;
 }
